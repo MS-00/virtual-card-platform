ca4399781741f03cb8e840586ab01932a5783b97
Mohit Sunda <sundamohit00@gmail.com>
2025-06-16 10:06:07 +0200

    Project setup

---
+++ src/main/java/com/mohit_sunda/virtual_card_platform/VirtualCardIssuancePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.mohit_sunda.virtual_card_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualCardIssuancePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualCardIssuancePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mohit_sunda/virtual_card_platform/VirtualCardIssuancePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohit_sunda.virtual_card_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualCardIssuancePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
